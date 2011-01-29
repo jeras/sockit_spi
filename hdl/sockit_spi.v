b7a90f6d69b7eb0e5eae37e6f5d30a707bef99f9
Iztok Jeras <iztok.jeras@gmail.com>
2011-01-30 00:06:46 +0100

    recoded slave select, output enable and byte counter

--- hdl/sockit_spi.v
+++ hdl/sockit_spi.v
@@ -64,18 +64,19 @@ module spi #(
 wire     [3:0] bus_dec;
 
 // configuration registers
-reg    [8-1:0] cfg_sse;     // slave select output enable
-reg    [8-1:0] cfg_sso;     // slave select output (active high)
-reg    [6-1:0] cfg_div;     // clock divider ratio
-reg    [2-1:0] cfg_xip;     // clock divider ratio
-reg            cfg_halt_e;  // hold output enable
-reg            cfg_halt_o;  // halt output
-reg            cfg_wp_e;    // write protect output enable
-reg            cfg_wp_o;    // write protect output
-reg            cfg_bit;     // bit mode
-reg            cfg_dir;     // shift direction (0 - lsb first, 1 - msb first)
-reg            cfg_cpol;    // clock polarity
-reg            cfg_cpha;    // clock shift
+reg    [8-1:0] cfg_sso;    // slave select outputs
+reg    [8-1:0] cfg_div;    // clock divider ratio
+reg    [4-1:0] cfg_xip;    // clock divider ratio
+reg            cfg_hle;    // hold output enable
+reg            cfg_hlo;    // hold output
+reg            cfg_wpe;    // write protect output enable
+reg            cfg_wpo;    // write protect output
+reg            cfg_sse;    // slave select output enable
+reg    [2-1:0] cfg_iom;    // IO mode (0-3wire, 1-SPI, 2-duo, 3-quad)
+reg            cfg_bit;    // bit mode
+reg            cfg_dir;    // shift direction (0 - lsb first, 1 - msb first)
+reg            cfg_pol;    // clock polarity
+reg            cfg_pha;    // clock phase
 
 // clock divider signals
 reg    [8-1:0] div_cnt;  // clock divider counter
@@ -83,16 +84,18 @@ wire           div_byp;
 reg            div_clk;  // register storing the SCLK clock value (additional division by two)
 
 // spi shifter signals
-reg  [32-1:0] reg_s;           // spi data shift register
-reg  reg_i, reg_o;             // spi input-sampling to output-change phase shift registers
-wire ser_i, ser_o;             // shifter serial input and output multiplexed signals
-wire spi_mi;                   //
-wire clk_l;                    // loopback clock
+reg   [32-1:0] reg_s;         // spi data shift register
+reg            reg_i, reg_o;  // spi input-sampling to output-change phase shift registers
+wire           ser_i, ser_o;  // shifter serial input and output multiplexed signals
+wire           spi_mi;        //
+wire           clk_l;         // loopback clock
 
 // spi shift transfer control registers
-reg          ctl_oen;  // 
-reg    [7:0] ctl_cnb;  // counter of transfered data units (bytes by default)
-reg    [2:0] cnt_bit;  // counter of shifted bits
+reg          ctl_ssc;  // slave select clear
+reg          ctl_sse;  // slave select clear
+reg          ctl_oen;  // data output enable
+reg    [7:0] ctl_cby;  // counter of bytes (default transfere units)
+reg    [2:0] ctl_cbt;  // counter of shifted bits
 wire         ctl_run;  // transfer running status
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -109,12 +112,16 @@ assign bus_dec [3] = (bus_adr == 2'h3);  // XIP base address
 ////////////////////////////////////////////////////////////////////////////////
 
 // output data multiplexer
-assign bus_rdt = bus_dec[0] ? reg_s :
-                 bus_dec[1] ? {23'h000000, ctl_oen, ctl_cnb} :
-                 bus_dec[2] ? {cfg_sse, cfg_sso, cfg_div, cfg_xip,
-                               cfg_halt_e, cfg_wp_e, cfg_halt_o, cfg_wp_o,
-                               cfg_bit, cfg_dir, cfg_cpol, cfg_cpha}
-                             : 32'hxxxxxxxx;
+assign bus_rdt = bus_dec[0] ? reg_s
+               : bus_dec[1] ? {23'h000000,
+                               ctl_ssc, ctl_cce, ctl_oen, ctl_cby}
+               : bus_dec[2] ? {                           cfg_sso,
+                                                          cfg_div,
+                               cfg_hle, cfg_wpe, cfg_hlo, cfg_wpo,
+                                                          cfg_xip,
+                                  1'b0, cfg_sse,          cfg_iom,
+                               cfg_bit, cfg_dir, cfg_pol, cfg_pha}
+               : 32'hxxxxxxxx;
 
 assign bus_wrq = 1'b0;
 assign bus_irq = 1'b0;
@@ -125,18 +132,28 @@ assign bus_irq = 1'b0;
 
 always @(posedge clk, posedge rst)
 if (rst) begin
-  {cfg_sse, cfg_sso, cfg_div, cfg_xip}         <= CFG_RST [31: 8];
-  {cfg_halt_e, cfg_wp_e, cfg_halt_o, cfg_wp_o} <= CFG_RST [ 7: 4];
-  {cfg_bit, cfg_dir, cfg_cpol, cfg_cpha}       <= CFG_RST [ 3: 0];
+  {                           cfg_sso} <= CFG_RST [31:24];
+  {                           cfg_div} <= CFG_RST [23:16];
+  {cfg_hle, cfg_wpe, cfg_hlo, cfg_wpo} <= CFG_RST [15:12];
+  {                           cfg_xip} <= CFG_RST [11: 8];
+           {cfg_sse,          cfg_iom} <= CFG_RST [ 6: 4];
+  {cfg_bit, cfg_dir, cfg_pol, cfg_pha} <= CFG_RST [ 3: 0];
 end else if (bus_wen & bus_dec[2] & ~bus_wrq) begin
-  {cfg_sse, cfg_sso, cfg_div, cfg_xip}         <= bus_wdt [31: 8];
-  {cfg_halt_e, cfg_wp_e, cfg_halt_o, cfg_wp_o} <= bus_wdt [ 7: 4];
-  {cfg_bit, cfg_dir, cfg_cpol, cfg_cpha}       <= bus_wdt [ 3: 0];
+  {                           cfg_sso} <= bus_wdt [31:24];
+  {                           cfg_div} <= bus_wdt [23:16];
+  {cfg_hle, cfg_wpe, cfg_hlo, cfg_wpo} <= bus_wdt [11: 8];
+  {                           cfg_xip} <= bus_wdt [11: 8];
+           {cfg_sse,          cfg_iom} <= bus_wdt [ 6: 4];
+  {cfg_bit, cfg_dir, cfg_pol, cfg_pha} <= bus_wdt [ 3: 0];
 end
 
+//////////////////////////////////////////////////////////////////////////////
+// clock divider                                                            //
+//////////////////////////////////////////////////////////////////////////////
+
 // spi slave select
-assign spi_ss_o = cfg_sso [SSW-1:0];
-assign spi_ss_e = cfg_sse [SSW-1:0];
+assign spi_ss_o = {SSW{ctl_sse}} & cfg_sso [SSW-1:0];
+assign spi_ss_e = {SSW{cfg_sse}};
 
 //////////////////////////////////////////////////////////////////////////////
 // clock divider                                                            //
@@ -159,11 +176,11 @@ end
 // clock output register (divider by 2)
 always @(posedge clk)
 if (~ctl_run)
-  div_clk <= cfg_cpol;
+  div_clk <= cfg_pol;
 else if (~|div_cnt)
   div_clk <= ~div_clk;
 
-assign div_ena = div_byp ? 1 : ~|div_cnt & (div_clk ^ cfg_cpol);
+assign div_ena = div_byp ? 1 : ~|div_cnt & (div_clk ^ cfg_pol);
 
 ////////////////////////////////////////////////////////////////////////////////
 // control/status registers (transfer counter and serial output enable)       //
@@ -171,30 +188,33 @@ assign div_ena = div_byp ? 1 : ~|div_cnt & (div_clk ^ cfg_cpol);
 
 // bit counter
 always @(posedge clk, posedge rst)
-if (rst)
-  cnt_bit <= 0;
-else if (ctl_run & div_ena)
-  cnt_bit <= cnt_bit + 1;
+if (rst)                     ctl_cbt <= 3'd0;
+else if (ctl_run & div_ena)  ctl_cbt <= ctl_cbt + 3'd1;
 
 // transfer length counter
 always @(posedge clk, posedge rst)
 if (rst) begin
+  ctl_ssc <= 1'b0;
+  ctl_sse <= 1'b0;
   ctl_oen <= 1'b0;
-  ctl_cnb <=  'd0;
+  ctl_cby <= 8'd0;
 end else begin
   // write from the CPU bus has priority
   if (bus_wen & bus_dec[1] & ~bus_wrq) begin
+    ctl_ssc <= bus_wdt[   10];
+    ctl_sse <= bus_wdt[    9];
     ctl_oen <= bus_wdt[    8];
-    ctl_cnb <= bus_wdt[ 7: 0];
+    ctl_cby <= bus_wdt[ 7: 0];
   // decrement at the end of each transfer unit (byte by default)
-  end else if (&cnt_bit & div_ena) begin
+  end else if (&ctl_cbt & div_ena) begin
+    ctl_sse <= ctl_sse & ~((ctl_cby == 8'd1) & ctl_ssc);
     ctl_oen <= 1'b1;
-    ctl_cnb <= ctl_cnb - 1;
+    ctl_cby <= ctl_cby - 8'd1;
   end
 end
 
 // spi transfer run status
-assign ctl_run = |ctl_cnb;
+assign ctl_run = |ctl_cby;
 
 ////////////////////////////////////////////////////////////////////////////////
 // spi shift register                                                         //
@@ -213,22 +233,22 @@ end
 assign ser_o  = (cfg_dir) ? reg_s [31] : reg_s [0];
 
 always @(posedge clk_l)
-if ( cfg_cpha)  reg_o <= ser_o;
+if ( cfg_pha)  reg_o <= ser_o;
 
 always @(posedge clk_l)
-if (~cfg_cpha)  reg_i <= spi_mi;
+if (~cfg_pha)  reg_i <= spi_mi;
 
 // spi clock output pin
-assign spi_sclk_o = div_byp ? cfg_cpol ^ (ctl_run & ~clk) : div_clk;
+assign spi_sclk_o = div_byp ? cfg_pol ^ (ctl_run & ~clk) : div_clk;
 
 // loop clock
-assign clk_l  = spi_sclk_i ^ cfg_cpol;
+assign clk_l  = spi_sclk_i ^ cfg_pol;
 
 // the serial input depends on the used protocol (SPI, 3 wire)
 assign spi_mi   = cfg_3wr ? spi_sio_i[0] : spi_sio_i[1];
 
-assign ser_i    = ~cfg_cpha ? reg_i : spi_mi;
-assign spi_sio_o[0] = ~cfg_cpha ? ser_o : reg_o;
+assign ser_i    = ~cfg_pha ? reg_i : spi_mi;
+assign spi_sio_o[0] = ~cfg_pha ? ser_o : reg_o;
 assign spi_sio_e[0] = ctl_oen;
 
 // temporary IO handler
@@ -237,7 +257,7 @@ assign spi_sclk_e   = 1'b1;
 assign spi_sio_o[1] = 1'bx;
 assign spi_sio_e[1] = 1'b0;
 
-assign spi_sio_o [3:2] = {cfg_halt_o, cfg_wp_o};
-assign spi_sio_e [3:2] = {cfg_halt_e, cfg_wp_e};
+assign spi_sio_o [3:2] = {cfg_hlo, cfg_wpo};
+assign spi_sio_e [3:2] = {cfg_hle, cfg_wpe};
 
 endmodule
--- hdl/spi_tb.v
+++ hdl/spi_tb.v
@@ -93,14 +93,12 @@ initial begin
   rst = 1'b0;
   repeat (4) @ (posedge clk);
 
-  // enable slave select
-  avalon_cycle (1, 4'h2, 4'hf, 32'hff00_0000, data);
   // write slave select and clock divider
-  avalon_cycle (1, 4'h2, 4'hf, 32'hff02_00f0, data);
+  avalon_cycle (1, 4'h2, 4'hf, 32'h0200_0f40, data);
   // write data register
   avalon_cycle (1, 4'h0, 4'hf, 32'h0123_4567, data);
   // write control register (enable a chip and start a 4 byte cycle)
-  avalon_cycle (1, 4'h1, 4'hf, 32'h8000_0108, data);
+  avalon_cycle (1, 4'h1, 4'hf, 32'h8000_708, data);
   repeat (500) @ (posedge clk);
   $finish();
 end
