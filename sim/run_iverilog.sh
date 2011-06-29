7c06c2a7a6c9737a43f4209cb29be46ec11cfd38
Iztok Jeras <iztok.jeras@gmail.com>
2011-06-29 14:04:11 +0200

    commented out coverage code for now

--- sim/run_iverilog.sh
+++ sim/run_iverilog.sh
@@ -9,6 +9,9 @@ rm -f flash.hex
 # convert Flash binary file into hexdecimal
 xxd -ps -c 1 flash.bin >> flash.hex
 
+# convert DMA memory binary file into hexadecimal
+xxd -ps -c 4 lorem-ipsum.txt >> dma_mem.hex
+
 # compile Verilog sources (testbench and RTL) with Icarus Verilog
 iverilog -o spi.out \
 ../hdl/spi_tb.v \
@@ -27,22 +30,22 @@ iverilog -o spi.out \
 #../hdl/MX25L12845E.v \
 #../hdl/s25fl129p00.v
 
-# compile verilog sources (testbench and RTL) for coverage
-#covered score -o spi.cdd -g 2 -t spi_tb \
-covered score -o spi.cdd -g 2 -t sockit_spi -i spi_tb.sockit_spi \
--v ../hdl/spi_tb.v \
--v ../hdl/sockit_spi.v \
--v ../hdl/sockit_spi_cdc.v \
--v ../hdl/sockit_spi_xip.v \
--v ../hdl/sockit_spi_dma.v \
--v ../hdl/spi_slave_model.v \
--v ../hdl/spi_flash_model.v
+# # compile verilog sources (testbench and RTL) for coverage
+# #covered score -o spi.cdd -g 2 -t spi_tb \
+# covered score -o spi.cdd -g 2 -t sockit_spi -i spi_tb.sockit_spi \
+# -v ../hdl/spi_tb.v \
+# -v ../hdl/sockit_spi.v \
+# -v ../hdl/sockit_spi_cdc.v \
+# -v ../hdl/sockit_spi_xip.v \
+# -v ../hdl/sockit_spi_dma.v \
+# -v ../hdl/spi_slave_model.v \
+# -v ../hdl/spi_flash_model.v
 
 # run the simulation
 vvp spi.out -fst
 
-# add simulation data to coverage database
-covered score -cdd spi.cdd -fst spi.fst -t sockit_spi -i spi_tb.sockit_spi
+# # add simulation data to coverage database
+# covered score -cdd spi.cdd -fst spi.fst -t sockit_spi -i spi_tb.sockit_spi
 
 # open the waveform and detach it
 gtkwave spi.fst sim_iverilog.sav &
